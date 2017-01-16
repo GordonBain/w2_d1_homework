fb0e6871b4c83ca568fdc6ca6cb6d76f55df8678
Gordon Bain <gbain8893@gmail.com>
2017-01-16 16:25:19 +0000

    files set up correctly with 0 runs 0 assertions

--- sports_team.rb
+++ sports_team.rb
@@ -1,8 +1,10 @@
 class SportsTeam
-  attr_accessor :
+  attr_accessor :team_name, :players, :coach
 
   def initialize(team_name, players, coach)
     @team = team_name
     @players = players
     @coach = coach
   end
+
+end
